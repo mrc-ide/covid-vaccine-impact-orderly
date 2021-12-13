53af88ef22ff081152f9817bfd06dbbff0a1f2b6
GBarnsley <gregbarnsley@hotmail.co.uk>
2021-12-13 17:31:26 +0000

    Allow loading to fits from One-Drive

--- src/fetch_fits/script.R
+++ src/fetch_fits/script.R
@@ -1,6 +1,24 @@
 ## First Get Data From Server
 
-fits <- get_fits(repo = repo, date = date, iso3cs = NULL, excess = excess)
+if(one_drive){
+  if(one_drive_loc == ""){
+    stop("Must specify one_drive_loc")
+  }
+  #load from onedrive location
+  loc <- file.path(one_drive_loc, ifelse(excess, "excess", "standard"))
+  #list files
+  files <- setdiff(unlist(strsplit(list.files(loc), "\\.")), "Rds")
+  #only keep countries
+  files <- intersect(files, squire::population$iso3c)
+  #load into one list
+  fits <- lapply(files, function(file){readRDS(file.path(loc, paste0(file, ".Rds")))})
+  names(fits) <- files
+} else {
+  if(repo == ""){
+    stop("Must specify repo")
+  }
+  fits <- get_fits(repo = repo, date = date, iso3cs = NULL, excess = excess)
+}
 
 #save fits in single Rds file
 saveRDS(fits, "countryfits.Rds")
