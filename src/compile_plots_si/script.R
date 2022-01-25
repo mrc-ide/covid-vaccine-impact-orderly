a34c7561856603ec9e72ebca27633bf3af328f5f
GBarnsley <gregbarnsley@hotmail.co.uk>
2022-01-25 20:21:36 +0000

    Add final SI compilation task

--- src/compile_plots_si/script.R
+++ src/compile_plots_si/script.R
@@ -1,3 +1,74 @@
 library(ggplot2)
 
 #delta sensitivity analysis
+no_res <- readRDS("usa_res.Rds")
+res <- readRDS("india_res.Rds")
+delta_fig <-
+  ggarrange(
+    nrow = 1,
+    ggplot(no_res, aes(x = delta_immune_escape, y = deaths_averted_median,
+                       ymin = deaths_averted_025, ymax = deaths_averted_975)) +
+      geom_line(colour = "red") + geom_ribbon(alpha = 0.1, fill = "red") +
+      #ylim(c(0, max(no_res$deaths_averted_975))) +
+      scale_x_continuous(labels = scales::percent) +
+      labs(x = "Delta Immune Escape", y = "Deaths Averted by Vaccinations\nMedian and 95% quantile") +
+      theme_pubr(),
+    ggplot(res, aes(x = delta_immune_escape, y = deaths_averted_median,
+                       ymin = deaths_averted_025, ymax = deaths_averted_975)) +
+      geom_line(colour = "purple") + geom_ribbon(alpha = 0.1, fill = "purple") +
+      #ylim(c(0, max(res$deaths_averted_975))) +
+      scale_x_continuous(labels = scales::percent) +
+      labs(x = "Delta Immune Escape", y = "Deaths Averted by Vaccinations\nMedian and 95% quantile") +
+      theme_pubr(),
+    labels = "auto"
+  )
+#reffs
+# no_reffs <- readRDS("usa_reff_plot.Rds")
+# reffs <- readRDS("india_reff_plot.Rds")
+# delta_eff_fig <- ggarrange(
+#   nrow = 1,
+# reffs %>%
+#   group_by(date, delta_immune_escape) %>%
+#   mutate(reff_loss = Reff-vacc_Reff) %>%
+#   summarise(reff_loss_avg = median(reff_loss),
+#             reff_loss_025 = quantile(reff_loss, 0.025),
+#             reff_loss_975 = quantile(reff_loss, 0.975)) %>%
+#   ggplot(
+#     aes(x = date, y = reff_loss_avg, ymin = reff_loss_025, ymax = reff_loss_975, group = delta_immune_escape,
+#         fill = delta_immune_escape)
+#   ) +
+#   geom_line(aes(colour = delta_immune_escape)) +
+#   geom_ribbon(alpha = 0.1) +
+#   labs(x = "Date", y = "Loss in Reff", fill = "Delta Immune Escape", colour = "Delta Immune Escape"),
+# no_reffs %>%
+#   group_by(date, delta_immune_escape) %>%
+#   mutate(reff_loss = Reff-vacc_Reff) %>%
+#   summarise(reff_loss_avg = median(reff_loss),
+#             reff_loss_025 = quantile(reff_loss, 0.025),
+#             reff_loss_975 = quantile(reff_loss, 0.975)) %>%
+#   ggplot(
+#     aes(x = date, y = reff_loss_avg, ymin = reff_loss_025, ymax = reff_loss_975, group = delta_immune_escape,
+#         fill = delta_immune_escape)
+#   ) +
+#   geom_line(aes(colour = delta_immune_escape)) +
+#   geom_ribbon(alpha = 0.1) +
+#   labs(x = "Date", y = "Loss in Reff", fill = "Delta Immune Escape", colour = "Delta Immune Escape"),
+# labels = "auto",  common.legend = TRUE
+# )
+ggsave("delta_sensitivity.png", delta_fig, width = 12, height = 7)
+ggsave("delta_sensitivity.pdf", delta_fig, width = 12, height = 7)
+
+#deaths averted excess
+averted_table <- readRDS("averted_table.Rds") %>%
+  rename(`Deaths Averted by Vaccinations\nTotal` = `Deaths Averted by Vaccinations`)
+write.csv(
+  averted_table,
+  "reported_averted_deaths_table.csv")
+
+#excess reported comparison
+reported_comparison <- readRDS("reported_excess_comparison.Rds")
+ggsave("reported_excess_comparison.png", reported_comparison$total, width = 8, height = 5)
+ggsave("reported_excess_comparison.pdf", reported_comparison$total, width = 8, height = 5)
+
+#hospital direct effects
+#WIP for now
--- src/deaths_averted_hospital/script.R
+++ src/deaths_averted_hospital/script.R
@@ -43,34 +43,32 @@ df_sorted %>%
   geom_line(aes(x = date, y = total_averted)) +
   geom_line(aes(x = date, y = averted_no_hosp))
 
-ggplot(df_sorted) +
+fig <- ggplot(df_sorted) +
   geom_line(aes(x = date, y = deaths_avg, colour = counterfactual))
 
-fig <- ggplot(data = df_overall %>%
-                   filter(date > "2021-01-1",
-                          counterfactual == "No Vaccines"),
-                 aes(x = date)) +
-  geom_area(
-    data = fig1_df_income %>%
-      filter(date > "2021-01-01"),
-    aes(x = date, y = averted_deaths_avg, fill = income_group),
-    alpha = 1
-  ) +
-  #geom_line(aes(y = averted_deaths_avg),
-  #          size = 1, colour = averted_col) +
-  #geom_ribbon(aes(ymin = averted_deaths_025,
-  #                ymax = averted_deaths_975),
-  #            alpha = 0.55,
-  #            fill = averted_col) +
-  labs(
-    x = "Date",
-    y = "Deaths Averted by Vaccinations per day",
-    fill = "Income Group:"
-  ) +
-  ylim(ggplot_build(fig1_1)$layout$panel_scales_y[[1]]$range$range - c(1,0)) +
-  theme_pubr() +
-  theme(legend.position = c(0.25, 0.8))
+# fig <- ggplot(data = df_overall %>%
+#                    filter(date > "2021-01-1",
+#                           counterfactual == "No Vaccines"),
+#                  aes(x = date)) +
+#   geom_area(
+#     data = fig1_df_income %>%
+#       filter(date > "2021-01-01"),
+#     aes(x = date, y = averted_deaths_avg, fill = income_group),
+#     alpha = 1
+#   ) +
+#   #geom_line(aes(y = averted_deaths_avg),
+#   #          size = 1, colour = averted_col) +
+#   #geom_ribbon(aes(ymin = averted_deaths_025,
+#   #                ymax = averted_deaths_975),
+#   #            alpha = 0.55,
+#   #            fill = averted_col) +
+#   labs(
+#     x = "Date",
+#     y = "Deaths Averted by Vaccinations per day",
+#     fill = "Income Group:"
+#   ) +
+#   ylim(ggplot_build(fig1_1)$layout$panel_scales_y[[1]]$range$range - c(1,0)) +
+#   theme_pubr() +
+#   theme(legend.position = c(0.25, 0.8))
 
-fig_1 <- ggarrange(fig1_1, fig1_2, labels = "auto")
-dir.create("plots", showWarnings = FALSE)
-saveRDS(fig_1, "plots/global_deaths_averted.Rds")
+saveRDS(fig, "hospital_effects.Rds")
