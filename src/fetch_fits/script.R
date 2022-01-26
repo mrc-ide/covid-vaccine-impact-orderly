53f9b87867aa254ff199ffaff4c380ce4a95f31e
GBarnsley <gregbarnsley@hotmail.co.uk>
2022-01-26 10:40:24 +0000

    Reduce fit size by removing chain results

--- src/fetch_fits/script.R
+++ src/fetch_fits/script.R
@@ -11,22 +11,31 @@ if(one_drive){
   #only keep countries
   files <- intersect(files, squire::population$iso3c)
   #load into one list
-  fits <- lapply(files, function(file){readRDS(file.path(loc, paste0(file, ".Rds")))})
-  #remove if not a realy fitted object
-  if(!excess){
-    fits <- lapply(seq_along(fits), function(x){
-      if("lmic_nimue_simulation" %in% class(fits[[x]])){
-        fits[[x]]
-      }
-    })
-  }
-  names(fits) <- files
+  names(files) <- files
+  fits <- map(files, ~readRDS(file.path(loc, paste0(.x, ".Rds"))))
 } else {
   if(repo == ""){
     stop("Must specify repo")
   }
   fits <- get_fits(repo = repo, date = date, iso3cs = NULL, excess = excess)
 }
+
+#remove any empty or non-fitted object+ remove chains
+fits <- map(fits, function(fit){
+  #pass checks to keep fit
+  if("lmic_nimue_simulation" %in% class(fit) |
+     "excess_nimue_simulation" %in% class(fit)){
+    #keep inputs
+    inputs <- fit$pmcmc_results$inputs
+    #remove mcmc chains
+    fit$pmcmc_results <- NULL
+    #re add inputs
+    fit$pmcmc_results <- list(
+      inputs = inputs
+    )
+    return(fit)
+  }
+}) %>% discard(is.null)
 
 #save fits in single Rds file
 saveRDS(fits, "countryfits.Rds")
